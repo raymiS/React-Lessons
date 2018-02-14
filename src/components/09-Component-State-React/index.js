64cbfba00abe3c235e8dbe497e865244476b5d8e
Raymi Saldomando <raymi@able.co>
2018-02-14 15:58:43 -0500

    React Lesson Nine

--- src/components/09-Component-State-React/index.js
+++ src/components/09-Component-State-React/index.js
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const LessonNine = () => (
-  <div className="container">
-    <h1>Lesson 9: Component State</h1>
-  </div>
-);
+class LessonNine extends Component {
+  state = { lapse: 0, running: false };
+  handleClearClick = () => {
+    clearInterval(this.timer);
+    this.setState({
+      lapse: 0,
+      running: false
+    });
+  };
+
+  handleRunClick = () => {
+    this.setState(state => {
+      if (state.running) {
+        clearInterval(this.timer);
+      } else {
+        const timeStart = Date.now() - this.state.lapse;
+        this.timer = setInterval(() => {
+          this.setState({
+            lapse: Date.now() - timeStart
+          });
+        });
+      }
+      return { running: !state.running };
+    });
+  };
+  render() {
+    const { lapse, running } = this.state;
+    const buttonStyles = {
+      border: '1px solid #ccc',
+      background: '#fff',
+      fontSize: '2em',
+      padding: 15,
+      margin: '10px auto',
+      width: 200,
+      display: 'block'
+    };
+    return (
+      <div className="container">
+        <h1>Lesson 9: Component State</h1>
+        <label style={{ display: 'block', fontSize: '2em' }}>{lapse}ms</label>
+        <button style={buttonStyles} onClick={this.handleRunClick}>
+          {running ? 'Stop' : 'Start'}
+        </button>
+        <button style={buttonStyles} onClick={this.handleClearClick}>
+          Clear
+        </button>
+      </div>
+    );
+  }
+}
 
 export default LessonNine;
