a890f9fe04e823acde79730a744d81ec80927e42
Raymi Saldomando <raymi@able.co>
2018-02-14 14:19:44 -0500

    React Lesson Nine

---
+++ src/components/09-Component-State-React/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LessonNine = () => (
+  <div className="container">
+    <h1>Lesson 9: Component State</h1>
+  </div>
+);
+
+export default LessonNine;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import './styles/base.css';
 
-export default function App() {
-  return <div>React Lessons</div>;
-}
+import LessonNine from './components/09-Component-State-React';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<LessonNine />, document.getElementById('root'));
 registerServiceWorker();
