ef42969d098c65007212fa22d8d65217462dd3a9
Raymi Saldomando <raymi@able.co>
2018-02-21 17:57:41 -0500

    React Lesson Fourteen

---
+++ src/components/14-Dynamic-Forms-React.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+class DynamicForms extends Component {
+  state = { error: null };
+  handleSubmit = event => {
+    event.preventDefault();
+    const value = event.target.elements.username.value;
+    const error = this.props.getErrorMessage(value);
+    if (error) {
+      alert(`error: ${error}`);
+    } else {
+      alert(`success: ${value}`);
+    }
+  };
+  handleSubmitChange = event => {
+    const { value } = event.target;
+    this.setState({
+      error: this.props.getErrorMessage(value)
+    });
+  };
+
+  // We use componentDidMount() life cycle method because at the moment this component is mounted the state of the error needs to be false, meaning that in the input text box, since there's nothing added yet, fails the valitation.
+
+  componentDidMount() {
+    this.setState({
+      error: this.props.getErrorMessage(' ')
+    });
+  }
+  render() {
+    const { error } = this.state;
+    return (
+      <div className="container">
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input
+              type="text"
+              name="username"
+              onChange={this.handleSubmitChange}
+            />
+          </label>
+          <button type="submit" disabled={Boolean(error)}>
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default DynamicForms;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,22 @@ import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import './styles/base.css';
 
-export default function App() {
-  return <div>React Lessons</div>;
-}
+import DynamicForms from './components/14-Dynamic-Forms-React';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <DynamicForms
+    getErrorMessage={value => {
+      if (value.length < 3) {
+        return `Value must be at least 3 characters, but is only ${
+          value.length
+        }`;
+      }
+      if (!value.includes('s')) {
+        return `Value does not include "s" but it should!`;
+      }
+      return null;
+    }}
+  />,
+  document.getElementById('root')
+);
 registerServiceWorker();
